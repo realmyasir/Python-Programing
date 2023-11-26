9747ec32353a97a2c641f34d925b0a3629d3e54b
Muhammad Yasir <145558820+realmyasir@users.noreply.github.com>
2023-11-26 12:30:02 +0500

    First Program

---
+++ Hello World.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 09:52:18 2023
+
+@author: Web 123
+"""
+print('Hello World !')
+print(7+8)
+print('one',end=' ')
+print('two',end=' ')
+print('three',end=' ')
+print('four',end=' ')
+print('One,\'two\',\"three\",four,\\five\\\n\tonce,\'I\' caught a\
+ fish\n\t\t\'//alive\\\\\'')
